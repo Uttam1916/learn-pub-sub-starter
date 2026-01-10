51941cec032dbb8594b0dc4b1995152bbb342d84
Uttam1916 <tired3120@gmail.com>
2026-01-10 14:48:52 +0530

    add: client side queue creation+binding

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	gamelogic "github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
+	pubsub "github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	routing "github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+var connectionURL = "amqp://guest:guest@127.0.0.1:5672/"
 
 func main() {
 	fmt.Println("Starting Peril client...")
+
+	conn, err := amqp.Dial(connectionURL)
+	if err != nil {
+		fmt.Println("Could not connect to RabbitMQ:", err)
+		return
+	}
+	defer conn.Close()
+
+	username, err := gamelogic.ClientWelcome()
+	if err != nil {
+		fmt.Println("Could not read username:", err)
+		return
+	}
+
+	queueName := routing.PauseKey + "." + username
+
+	ch, _, err := pubsub.DeclareAndBind(
+		conn,
+		routing.ExchangePerilDirect,
+		queueName,
+		routing.PauseKey,
+		pubsub.Transient,
+	)
+	if err != nil {
+		fmt.Println("Failed to declare and bind queue:", err)
+		return
+	}
+	defer ch.Close()
+
+	// Wait for Ctrl+C
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt)
+	<-sig
 }
--- internal/pubsub/pubsub.go
+++ internal/pubsub/pubsub.go
@@ -8,6 +8,13 @@ import (
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
+type SimpleQueueType bool
+
+const (
+	Durable   SimpleQueueType = true
+	Transient SimpleQueueType = false
+)
+
 func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
 	json, err := json.Marshal(val)
 	if err != nil {
@@ -17,4 +24,49 @@ func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
 
 	ch.PublishWithContext(context.Background(), exchange, key, false, false, amqp.Publishing{ContentType: "application/json", Body: json})
 	return nil
+}
+
+func DeclareAndBind(
+	conn *amqp.Connection,
+	exchange,
+	queueName,
+	key string,
+	queueType SimpleQueueType,
+) (*amqp.Channel, amqp.Queue, error) {
+
+	chnl, err := conn.Channel()
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+
+	durable := queueType == Durable
+	exclusive := queueType == Transient
+	autoDelete := queueType == Transient
+
+	queue, err := chnl.QueueDeclare(
+		queueName,
+		durable,
+		autoDelete,
+		exclusive,
+		false,
+		nil,
+	)
+	if err != nil {
+		chnl.Close()
+		return nil, amqp.Queue{}, err
+	}
+
+	err = chnl.QueueBind(
+		queue.Name,
+		key,
+		exchange,
+		false,
+		nil,
+	)
+	if err != nil {
+		chnl.Close()
+		return nil, amqp.Queue{}, err
+	}
+
+	return chnl, queue, nil
 }
