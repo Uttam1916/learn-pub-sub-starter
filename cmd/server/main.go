6ed55911d2b126bd073b473a2a2663f53e7c7cfa
Uttam1916 <tired3120@gmail.com>
2026-01-13 00:19:21 +0530

    add: REPL serverside

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,39 +2,72 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"os/signal"
-	"syscall"
 
+	gamelogic "github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
 	pubsub "github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
 	routing "github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
-var connection string = "amqp://guest:guest@127.0.0.1:5672/"
+var connectionURL = "amqp://guest:guest@127.0.0.1:5672/"
 
 func main() {
 	fmt.Println("Starting Peril server...")
 
-	con, err := amqp.Dial(connection)
+	conn, err := amqp.Dial(connectionURL)
 	if err != nil {
 		fmt.Println("Failed to connect to RabbitMQ:", err)
 		return
 	}
-	defer con.Close()
+	defer conn.Close()
 
-	chnl, err := con.Channel()
+	ch, err := conn.Channel()
 	if err != nil {
-		fmt.Println("Failed to create channel", err)
+		fmt.Println("Failed to create channel:", err)
 		return
 	}
+	defer ch.Close()
 
-	fmt.Println("connection successful")
+	gamelogic.PrintServerHelp()
 
-	pubsub.PublishJSON(chnl, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: true})
+	for {
+		words := gamelogic.GetInput()
+		if len(words) == 0 {
+			continue
+		}
 
-	sigCh := make(chan os.Signal, 1)
-	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
-	<-sigCh
-	fmt.Println("closing program")
+		switch words[0] {
+
+		case "pause":
+			fmt.Println("Sending pause message")
+			err := pubsub.PublishJSON(
+				ch,
+				routing.ExchangePerilDirect,
+				routing.PauseKey,
+				routing.PlayingState{IsPaused: true},
+			)
+			if err != nil {
+				fmt.Println("Failed to publish pause:", err)
+			}
+
+		case "resume":
+			fmt.Println("Sending resume message")
+			err := pubsub.PublishJSON(
+				ch,
+				routing.ExchangePerilDirect,
+				routing.PauseKey,
+				routing.PlayingState{IsPaused: false},
+			)
+			if err != nil {
+				fmt.Println("Failed to publish resume:", err)
+			}
+
+		case "quit":
+			fmt.Println("Exiting server")
+			return
+
+		default:
+			fmt.Println("Unknown command:", words[0])
+		}
+	}
 }
